8c2fd43b1b50418e789f51dfce5236ac886296dd
Esther Olatunde <olatundeesther@gmail.com>
2018-01-03 16:02:03 +0100

    Restructures files

---
+++ esther/set1/3_xor_cipher.rb
@@ -0,0 +1,13 @@
+input = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+
+# score english text by frequency
+
+# cipher text ^ plain text = key
+
+def score_english_text(str)
+  
+end
+
+the_alphabets = ("a".."z").to_a
+
+plain_text.each do |a|
